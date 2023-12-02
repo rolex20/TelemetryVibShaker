0e6dc64f4097c11cb3e1ec7d1b5a598ddc6f22d6
Rolando <rolando.lopez@outlook.com>
2023-12-02 17:08:00 -0600

    Initial build of WAV Test Player

---
+++ WAV Test Player/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WAV_Test_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWAVFile = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoundDevice = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tbVolume = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Sound File";
+            // 
+            // txtWAVFile
+            // 
+            this.txtWAVFile.Location = new System.Drawing.Point(139, 77);
+            this.txtWAVFile.Name = "txtWAVFile";
+            this.txtWAVFile.Size = new System.Drawing.Size(261, 20);
+            this.txtWAVFile.TabIndex = 1;
+            this.txtWAVFile.Text = "C:\\Windows\\Media\\tada.wav";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(139, 45);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Repeat";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Sound Device";
+            // 
+            // txtSoundDevice
+            // 
+            this.txtSoundDevice.Location = new System.Drawing.Point(139, 114);
+            this.txtSoundDevice.Name = "txtSoundDevice";
+            this.txtSoundDevice.Size = new System.Drawing.Size(261, 20);
+            this.txtSoundDevice.TabIndex = 4;
+            this.txtSoundDevice.Text = "Speakers (High Definition Audio Device)";
+            this.txtSoundDevice.TextChanged += new System.EventHandler(this.txtSoundDevice_TextChanged);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(406, 77);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(63, 27);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(406, 114);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(63, 27);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tbVolume
+            // 
+            this.tbVolume.LargeChange = 10;
+            this.tbVolume.Location = new System.Drawing.Point(139, 153);
+            this.tbVolume.Maximum = 100;
+            this.tbVolume.Name = "tbVolume";
+            this.tbVolume.Size = new System.Drawing.Size(261, 45);
+            this.tbVolume.TabIndex = 7;
+            this.tbVolume.TickFrequency = 10;
+            this.tbVolume.Value = 100;
+            this.tbVolume.Scroll += new System.EventHandler(this.tbVolume_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Volume";
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(406, 153);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(33, 13);
+            this.lblVolume.TabIndex = 9;
+            this.lblVolume.Text = "100%";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnPlay;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 246);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbVolume);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.txtSoundDevice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtWAVFile);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "WAV Test Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWAVFile;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoundDevice;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TrackBar tbVolume;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblVolume;
+    }
+}
+
---
+++ WAV Test Player/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+
+namespace WAV_Test_Player
+{
+    public partial class Form1 : Form
+    {
+
+        WaveOut waveOut;
+        AudioFileReader waveProvider;
+        
+
+        int deviceIndex = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            // create a device enumerator
+            var enumerator = new MMDeviceEnumerator();
+
+            // get the index of the device named "Output Sound Device 2"
+            int deviceIndex = -1;
+            for (int i = 0; i < WaveOut.DeviceCount; i++)
+            {
+                var device = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active)[i];
+
+                //Console.WriteLine("Device ID[{0}] = [{1}]", i, device.FriendlyName);
+                if (device.FriendlyName == txtSoundDevice.Text)
+                {
+                    deviceIndex = i;
+                    break;
+                }
+            }
+
+            // check if the device was found
+            if (deviceIndex == -1)
+            {
+                MessageBox.Show("Audio device not found");
+                return;
+            }
+
+
+            // create a wave out object and set the device number
+            waveOut = new WaveOut();
+            waveOut.DeviceNumber = deviceIndex;
+
+            // create a wave provider from a file
+
+            var waveProvider = new AudioFileReader(txtWAVFile.Text);
+
+
+            // create a loop stream from the wave provider
+            var loopStream = new LoopStream(waveProvider);
+
+            // initialize the wave out object with the loop stream
+            waveOut.Init(loopStream);
+
+
+            // set the initial volume to 100%
+            waveOut.Volume = tbVolume.Value / 100.0f;
+
+
+            waveOut.Play();
+
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (waveOut != null) { 
+            waveOut.Stop();
+            waveOut.Dispose();
+            waveOut = null;
+            }
+        }
+
+        private void txtSoundDevice_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbVolume_Scroll(object sender, EventArgs e)
+        {
+            lblVolume.Text = tbVolume.Value.ToString() + "%";
+            if (waveOut != null)
+                waveOut.Volume = tbVolume.Value / 100.0f; ;
+        }  
+    }
+}
