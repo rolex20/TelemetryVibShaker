d88f3c83eebe3b3676bf3219ae83a647855d6175
Rolando <rolando.lopez@outlook.com>
2024-05-17 00:19:41 -0600

    60% completed

--- RemoteWindowControl/Form1.Designer.cs
+++ RemoteWindowControl/Form1.Designer.cs
@@ -30,49 +30,277 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            nudInstance = new NumericUpDown();
+            btnReadPosition = new Button();
+            btnSendChanges = new Button();
+            rbRestore = new RadioButton();
+            rbMinimize = new RadioButton();
+            rbNoChange = new RadioButton();
+            label2 = new Label();
+            txtYCoord = new TextBox();
+            cmbProcesses = new ComboBox();
+            label1 = new Label();
+            txtXCoord = new TextBox();
             tabPage2 = new TabPage();
+            lblLink = new LinkLabel();
+            lblWebServerThreadId = new Label();
+            txtPort = new TextBox();
+            txtIPAddress = new TextBox();
+            tabPage3 = new TabPage();
+            txtErrors = new TextBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
             tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nudInstance).BeginInit();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
             // 
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
             tabControl1.Location = new Point(30, 34);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(200, 100);
+            tabControl1.Size = new Size(393, 352);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(nudInstance);
+            tabPage1.Controls.Add(btnReadPosition);
+            tabPage1.Controls.Add(btnSendChanges);
+            tabPage1.Controls.Add(rbRestore);
+            tabPage1.Controls.Add(rbMinimize);
+            tabPage1.Controls.Add(rbNoChange);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(txtYCoord);
+            tabPage1.Controls.Add(cmbProcesses);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(txtXCoord);
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(192, 72);
+            tabPage1.Size = new Size(385, 324);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "tabPage1";
             tabPage1.UseVisualStyleBackColor = true;
             // 
+            // nudInstance
+            // 
+            nudInstance.Location = new Point(244, 25);
+            nudInstance.Name = "nudInstance";
+            nudInstance.Size = new Size(120, 23);
+            nudInstance.TabIndex = 10;
+            // 
+            // btnReadPosition
+            // 
+            btnReadPosition.Location = new Point(183, 235);
+            btnReadPosition.Name = "btnReadPosition";
+            btnReadPosition.Size = new Size(107, 23);
+            btnReadPosition.TabIndex = 9;
+            btnReadPosition.Text = "Read Position";
+            btnReadPosition.UseVisualStyleBackColor = true;
+            btnReadPosition.Click += btnReadPosition_Click;
+            // 
+            // btnSendChanges
+            // 
+            btnSendChanges.Location = new Point(40, 235);
+            btnSendChanges.Name = "btnSendChanges";
+            btnSendChanges.Size = new Size(106, 23);
+            btnSendChanges.TabIndex = 8;
+            btnSendChanges.Text = "Send Changes";
+            btnSendChanges.UseVisualStyleBackColor = true;
+            btnSendChanges.Click += btnSendChanges_Click;
+            // 
+            // rbRestore
+            // 
+            rbRestore.AutoSize = true;
+            rbRestore.Location = new Point(244, 179);
+            rbRestore.Name = "rbRestore";
+            rbRestore.Size = new Size(64, 19);
+            rbRestore.TabIndex = 7;
+            rbRestore.Text = "Restore";
+            rbRestore.UseVisualStyleBackColor = true;
+            // 
+            // rbMinimize
+            // 
+            rbMinimize.AutoSize = true;
+            rbMinimize.Location = new Point(153, 179);
+            rbMinimize.Name = "rbMinimize";
+            rbMinimize.Size = new Size(74, 19);
+            rbMinimize.TabIndex = 6;
+            rbMinimize.Text = "Minimize";
+            rbMinimize.UseVisualStyleBackColor = true;
+            // 
+            // rbNoChange
+            // 
+            rbNoChange.AutoSize = true;
+            rbNoChange.Checked = true;
+            rbNoChange.Location = new Point(40, 179);
+            rbNoChange.Name = "rbNoChange";
+            rbNoChange.Size = new Size(85, 19);
+            rbNoChange.TabIndex = 5;
+            rbNoChange.TabStop = true;
+            rbNoChange.Text = "No Change";
+            rbNoChange.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(35, 123);
+            label2.Name = "label2";
+            label2.Size = new Size(14, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Y";
+            // 
+            // txtYCoord
+            // 
+            txtYCoord.Location = new Point(71, 120);
+            txtYCoord.Name = "txtYCoord";
+            txtYCoord.Size = new Size(100, 23);
+            txtYCoord.TabIndex = 3;
+            txtYCoord.Text = "15";
+            // 
+            // cmbProcesses
+            // 
+            cmbProcesses.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbProcesses.FormattingEnabled = true;
+            cmbProcesses.Items.AddRange(new object[] { "Notepad", "calc.exe", "explorer.exe" });
+            cmbProcesses.Location = new Point(71, 24);
+            cmbProcesses.Name = "cmbProcesses";
+            cmbProcesses.Size = new Size(121, 23);
+            cmbProcesses.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(35, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(14, 15);
+            label1.TabIndex = 1;
+            label1.Text = "X";
+            // 
+            // txtXCoord
+            // 
+            txtXCoord.Location = new Point(71, 80);
+            txtXCoord.Name = "txtXCoord";
+            txtXCoord.Size = new Size(100, 23);
+            txtXCoord.TabIndex = 0;
+            txtXCoord.Text = "10";
+            // 
             // tabPage2
             // 
+            tabPage2.Controls.Add(lblLink);
+            tabPage2.Controls.Add(lblWebServerThreadId);
+            tabPage2.Controls.Add(txtPort);
+            tabPage2.Controls.Add(txtIPAddress);
             tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(192, 72);
+            tabPage2.Size = new Size(385, 324);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "tabPage2";
             tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lblLink
+            // 
+            lblLink.AutoSize = true;
+            lblLink.Location = new Point(109, 197);
+            lblLink.Name = "lblLink";
+            lblLink.Size = new Size(60, 15);
+            lblLink.TabIndex = 3;
+            lblLink.TabStop = true;
+            lblLink.Text = "linkLabel1";
+            // 
+            // lblWebServerThreadId
+            // 
+            lblWebServerThreadId.AutoSize = true;
+            lblWebServerThreadId.Location = new Point(103, 144);
+            lblWebServerThreadId.Name = "lblWebServerThreadId";
+            lblWebServerThreadId.Size = new Size(22, 15);
+            lblWebServerThreadId.TabIndex = 2;
+            lblWebServerThreadId.Text = "---";
+            // 
+            // txtPort
+            // 
+            txtPort.Location = new Point(103, 83);
+            txtPort.Name = "txtPort";
+            txtPort.Size = new Size(126, 23);
+            txtPort.TabIndex = 1;
+            txtPort.Text = "8081";
+            txtPort.TextChanged += txtPort_TextChanged;
+            // 
+            // txtIPAddress
+            // 
+            txtIPAddress.Location = new Point(103, 39);
+            txtIPAddress.Name = "txtIPAddress";
+            txtIPAddress.Size = new Size(126, 23);
+            txtIPAddress.TabIndex = 0;
+            txtIPAddress.Text = "192.168.1.17";
+            txtIPAddress.TextChanged += txtIPAddress_TextChanged;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(txtErrors);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(385, 324);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "tabPage3";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // txtErrors
+            // 
+            txtErrors.Location = new Point(24, 46);
+            txtErrors.Multiline = true;
+            txtErrors.Name = "txtErrors";
+            txtErrors.ReadOnly = true;
+            txtErrors.Size = new Size(325, 248);
+            txtErrors.TabIndex = 0;
+            txtErrors.Text = "xxx";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(46, 279);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(146, 23);
+            textBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(215, 279);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 12;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // frmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(477, 429);
             Controls.Add(tabControl1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
             Name = "frmMain";
             Text = "Window Remote Control";
+            Load += frmMain_Load;
             tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)nudInstance).EndInit();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -81,5 +309,24 @@
         private TabControl tabControl1;
         private TabPage tabPage1;
         private TabPage tabPage2;
+        private Label label1;
+        private TextBox txtXCoord;
+        private Label label2;
+        private TextBox txtYCoord;
+        private ComboBox cmbProcesses;
+        private RadioButton rbMinimize;
+        private RadioButton rbNoChange;
+        private RadioButton rbRestore;
+        private Button btnSendChanges;
+        private Button btnReadPosition;
+        private NumericUpDown nudInstance;
+        private TextBox txtIPAddress;
+        private TextBox txtPort;
+        private TabPage tabPage3;
+        private TextBox txtErrors;
+        private Label lblWebServerThreadId;
+        private LinkLabel lblLink;
+        private Button button1;
+        private TextBox textBox1;
     }
 }
--- RemoteWindowControl/Form1.cs
+++ RemoteWindowControl/Form1.cs
@@ -1,10 +1,323 @@
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+
 namespace RemoteWindowControl
 {
     public partial class frmMain : Form
     {
+        private HttpListener listener; // Web Server for remote control location and focus commands
+        private int webServerThreadId, dispatcherUIThread;
+        private int ExCounter = 0;  // Exceptions Counter
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetCurrentThreadId();
+
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOWNORMAL = 1;
+        private const int SW_SHOWMINIMIZED = 2;
+        private const int SW_SHOWMAXIMIZED = 3;
+        private const int SW_RESTORE = 9;
+
+        public static void MinimizeWindow(IntPtr hWnd)
+        {
+            ShowWindow(hWnd, SW_SHOWMINIMIZED);
+        }
+
+        public static void MaximizeWindow(IntPtr hWnd)
+        {
+            ShowWindow(hWnd, SW_SHOWMAXIMIZED);
+        }
+
+        public static void RestoreWindow(IntPtr hWnd)
+        {
+            ShowWindow(hWnd, SW_RESTORE);
+        }
+
+        // Define the RECT structure
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        // Define the POINT structure
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        // Define the WINDOWPLACEMENT structure
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WINDOWPLACEMENT
+        {
+            public int length;
+            public int flags;
+            public int showCmd;
+            public POINT ptMinPosition;
+            public POINT ptMaxPosition;
+            public RECT rcNormalPosition;
+            // The following field is not used in the .NET definition
+            // public RECT rcDevice;
+        }
+
+        // Declare the GetWindowRect function
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+
+
+        // Declare the GetWindowPlacement function
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOZORDER = 0x0004;
+
+        public static void MoveWindowToCoordinates(IntPtr hWnd, int x, int y)
+        {
+            SetWindowPos(hWnd, IntPtr.Zero, x, y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+        }
+
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private IntPtr GetHandleByProcessName(string ProcessName, int Instance)
+        {
+            Process[] processes = Process.GetProcessesByName(ProcessName);
+            IntPtr windowHandle = processes[Instance].MainWindowHandle;
+            return windowHandle;
+        }
+
+        private void btnSendChanges_Click(object sender, EventArgs e)
+        {
+
+            IntPtr windowHandle = GetHandleByProcessName(cmbProcesses.Items[cmbProcesses.SelectedIndex].ToString(), (int)nudInstance.Value);
+            MoveWindowToCoordinates(windowHandle, Convert.ToInt32(txtXCoord.Text), Convert.ToInt32(txtYCoord.Text));
+
+            if (rbMinimize.Checked)
+                MinimizeWindow(windowHandle);
+            else if (rbRestore.Checked)
+                RestoreWindow(windowHandle);
+
+        }
+
+        private void btnReadPosition_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName(cmbProcesses.Items[cmbProcesses.SelectedIndex].ToString());
+            IntPtr windowHandle = processes[0].MainWindowHandle;
+
+            RECT windowRect = new RECT();
+            GetWindowRect(windowHandle, out windowRect);
+            int x = windowRect.Left;
+            int y = windowRect.Top;
+
+            txtXCoord.Text = x.ToString();
+            txtYCoord.Text = y.ToString();
+        }
+
+        private void StartWebServer()
+        {
+            listener = new HttpListener();
+            string prefix = $"http://{txtIPAddress.Text}:{txtPort.Text}/";
+            listener.Prefixes.Add(prefix);
+            //listener.Prefixes.Add("http://perfmon:8080/");
+            //listener.Prefixes.Add("http://192.168.1.5:8080/"); // remove this line when debugging
+            //listener.Prefixes.Add("http://localhost:8080/");
+            try
+            {
+                listener.Start();
+            }
+            catch (Exception ex)
+            {
+                ExCounter++;
+                LogError(ex.Message, "StartWebServer{ listener.Start(); }");
+            }
+            Task.Run(() => ProcessRequest());
+        }
+
+        private async Task ProcessRequest()
+        {
+            while (true)
+            {
+                var context = await listener.GetContextAsync();
+                var request = context.Request;
+                var response = context.Response;
+
+                webServerThreadId = (int)GetCurrentThreadId();
+                lblWebServerThreadId.Text = webServerThreadId.ToString();
+
+
+                if (request.HttpMethod == "POST")
+                {
+                    using (var reader = new StreamReader(request.InputStream, Encoding.UTF8))
+                    {
+                        string formData = await reader.ReadToEndAsync();
+
+                        var parameters = formData.Split('&')
+                                                .Select(param => param.Split('='))
+                                                .ToDictionary(param => param[0], param => WebUtility.UrlDecode(param[1]));
+
+                        string sx, sy;
+                        int x = -1;
+                        int y = -1;
+                        if (parameters.TryGetValue("x", out sx) && parameters.TryGetValue("y", out sy))
+                        {
+                            x = int.Parse(sx);
+                            y = int.Parse(sy);
+                        }
+
+                        string topMost, focus, reset, exit;
+
+                        parameters.TryGetValue("topmost", out topMost);
+                        parameters.TryGetValue("focus", out focus);
+                        parameters.TryGetValue("reset", out reset);
+                        parameters.TryGetValue("exit", out exit);
+
+
+                        MakeFormChanges(x, y, topMost, focus, reset);
+
+                        SendResponse(response, $"{DateTime.Now.ToString("[dd/MM/yyyy HH:mm:ss]")} Successfully requested coordinates change to X: {x}, Y: {y}");
+
+                        if (exit != null) Application.Exit();
+                    }
+                }
+                else
+                {
+                    string timestamp = DateTime.Now.ToString("[dd/MM/yyyy HH:mm:ss]");
+                    SendResponse(response, "Welcome! Please submit new X and Y coordinates.");
+                }
+            }
+        }
+
+        private void MakeFormChanges(int x, int y, string topMost, string focus, string reset)
+        {
+            this.Invoke(new Action(() =>
+            {
+                //dispatcherUIThread = (int)GetCurrentThreadId();
+
+
+                //txtErrors.AppendText($"Moving form to x={x}, y={y}" + Environment.NewLine);
+
+                //if (x >= 0 && y >= 0)
+                this.Location = new System.Drawing.Point(x, y);
+
+                // If the user submitted the form with topMost empty, this means the user might want to set this property to false
+                this.TopMost = topMost != null;
+
+                if (focus != null) this.Activate();
+
+                if (reset != null) LogError("reset was called", "MakeFormChanges()");
+
+            }));
+        }
+
+        private void SendResponse(HttpListenerResponse response, string message)
+        {
+
+            string marked = (this.TopMost ? "checked" : String.Empty);
+            string responseString = $@"
+                <html>
+                <head><title>Performance Monitor Light - Web Server</title>
+                <link rel=""stylesheet"" href=""https://learn.microsoft.com/_themes/docs.theme/master/en-us/_themes/styles/24b6bbbc.site-ltr.css "">
+                </head>
+                <body leftmargin='10' topmargin='10'>
+                    <h1 align='center'>{message}</h1>
+                    <table border='1' align='center'><tr><td>
+                    <form method='post' align='center'>
+                        <br><br><label for='x'>X:</label>
+                        <input type='number' name='x' value='{this.Location.X}' required placeholder='type new X coordinate'/><br><br>
+                        <label for='y'>Y:</label>
+                        <input type='number' name='y' value='{this.Location.Y}' required placeholder='type new Y coordinate' autofocus/><br><br>
+
+                        <input type='checkbox' id='topmost' name='topmost' value='always_on_top' {marked}>
+                        <label for='topmost'> Always on top</label><br>
+
+                        <input type='checkbox' id='focus' name='focus' value='focus'>
+                        <label for='focus'>Focus</label><br>
+
+                        
+                        <input type='checkbox' id='reset' name='reset' value='Reset'>
+                        <label for='reset'>Reset All Max Values</label><br>
+                        
+                        <input type='checkbox' id='exit' name='exit' value='Exit'>
+                        <label for='exit'>Exit program</label><br>
+
+                        <input type='submit' value='Submit' />
+                    </form>
+                    <hr>{DateTime.Now.ToString("[dd/MM/yyyy HH:mm:ss]")} WebServerThreadID: {webServerThreadId.ToString()}
+                </td</tr><table>
+                </body>
+                </html>";
+
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            using (var output = response.OutputStream)
+            {
+                output.Write(buffer, 0, buffer.Length);
+            }
+            response.Close();
+        }
+
+        private void LogError(string message, string function)
+        {
+            if (ExCounter <= 100) // Only log the first 100 errors
+            {
+                // Get the current timestamp
+                string timestamp = DateTime.Now.ToString("HH:mm:ss.fff");
+
+                // Format the error message with the timestamp
+                string errorMessage = $"[{timestamp}] [{function}] {message}{Environment.NewLine}";
+
+                // Check if the action is being called from a thread other than the UI thread
+                if (txtErrors.InvokeRequired)
+                {
+                    txtErrors.Invoke(new Action(() => txtErrors.AppendText(errorMessage)));
+                }
+                else
+                {
+                    txtErrors.AppendText(errorMessage);
+                }
+            }
+        }
+
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            StartWebServer();
+        }
+
+        private void txtIPAddress_TextChanged(object sender, EventArgs e)
+        {
+            lblLink.Text = $"http://{txtIPAddress.Text}:{txtPort.Text}/";
+        }
+
+        private void txtPort_TextChanged(object sender, EventArgs e)
+        {
+            lblLink.Text = $"http://{txtIPAddress.Text}:{txtPort.Text}/";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LogError(textBox1.Text, "button1_Click()");
         }
     }
 }
