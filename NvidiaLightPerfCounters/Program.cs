ee3e14755de6132d6972ec67b7f85f3fac1dd866
Rolando <rolando.lopez@outlook.com>
2024-08-07 02:38:33 -0600

    Initial version to test

--- NvidiaLightPerfCounters/Program.cs
+++ NvidiaLightPerfCounters/Program.cs
@@ -1,25 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// Based-on and full credits to: https://github.com/Alexey-Kamenev/GpuPerfCounters
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NvidiaLightPerfCounters
 {
-    internal static class Program
+    [RunInstaller(true)]
+    public class GpuPerfCountersInstaller : Installer
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main()
+        private readonly ServiceInstaller _svcInst;
+        private readonly ServiceProcessInstaller _procInst;
+
+        public GpuPerfCountersInstaller()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            _procInst = new ServiceProcessInstaller { Account = ServiceAccount.LocalSystem, };
+
+            _svcInst = new ServiceInstaller
             {
-                new PerfCounterService()
+                StartType = ServiceStartMode.Automatic,
+                ServiceName = "NvidiaLightPerfCounters",
+                DisplayName = "Nvidia-GPU Performance Counters",
+                Description = "Enables monitoring NVIDIA GPU performance counters in tools like perfmon and others."
             };
-            ServiceBase.Run(ServicesToRun);
+
+            Installers.Add(_svcInst);
+            Installers.Add(_procInst);
+        }
+
+        protected override void OnAfterUninstall(IDictionary savedState)
+        {
+            PerformanceCounterCategory.Delete(PerfCounterService.CategoryName);
+
+            base.OnAfterUninstall(savedState);
         }
     }
+
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 1 && (args[0] == "-install" || args[0] == "-uninstall"))
+            {
+                switch (args[0])
+                {
+                    case "-install":
+                        Install(false);
+                        break;
+                    case "-uninstall":
+                        Install(true);
+                        break;
+                }
+            }
+            else
+                ServiceBase.Run(new PerfCounterService());
+        }
+
+        private static void Install(bool uninstall)
+        {
+            AssemblyInstaller inst = new AssemblyInstaller(typeof(Program).Assembly, null);
+            IDictionary state = new Hashtable();
+            try
+            {
+                inst.UseNewContext = true;
+                if (!uninstall)
+                {
+                    inst.Install(state);
+                    inst.Commit(state);
+                }
+                else
+                    inst.Uninstall(state);
+            }
+            catch (Exception)
+            {
+                inst.Rollback(state);
+                throw;
+            }
+        }
+    }
+
+
 }
