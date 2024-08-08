4b8bacd31269c5290c6893d88ebf10d0f43b67c3
Rolando <rolando.lopez@outlook.com>
2024-08-08 02:55:11 -0600

    Created

---
+++ WarThunderMissionGenerator/warthunder/ps_scripts/Send-MessageViaPipe.ps1
@@ -0,0 +1,27 @@
+﻿function Send-MessageViaPipe {
+    param (
+        [string]$pipeName,
+        [string]$message
+    )
+
+    $pipePath = "\\.\pipe\$pipeName"
+
+    try {
+        $pipe = New-Object System.IO.Pipes.NamedPipeClientStream(".", $pipeName, [System.IO.Pipes.PipeDirection]::Out)
+        $pipe.Connect(1000)
+
+        $writer = New-Object System.IO.StreamWriter($pipe)
+        $writer.AutoFlush = $true
+        $writer.WriteLine($message)
+        $writer.Close()
+        $pipe.Close()
+
+        Write-Output "Message sent successfully."
+    } catch {
+        Write-Error "Failed to send message: $_"
+    }
+}
+
+#You can use this function like this:
+
+Send-MessageViaPipe -pipeName "MyPipe" -message "Hello, World!"
